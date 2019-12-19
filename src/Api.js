9dedc7b36dcad86cbf31102598107650b3ff3e6c
Sunil Malviya <sunilmalviya.codingkart@mail.ccom>
2019-12-19 08:34:18 +0530

    api ssl upate

--- src/Api.js
+++ src/Api.js
@@ -5,6 +5,7 @@ const WooCommerce  = new WooCommerceAPI({
   consumerKey: 'ck_03e83242fdcbb62a01daebe9c4817741f4c18a36', // Your consumer secret
   consumerSecret: 'cs_069ce05d83a89152f983fcdd2d4460c213256009', // Your consumer secret
   wpAPI: true, // Enable the WP REST API integration
+  verifySsl: false,
   version: 'wc/v1', // WooCommerce WP REST API version
   
 });
